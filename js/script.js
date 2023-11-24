8fe069d9e512819a76886500bef19ad6df0de624
Faheem <66919065+fahboa@users.noreply.github.com>
2023-11-24 16:50:51 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Detecting button press
+var drumButtons = document.querySelectorAll(".drum");
+
+drumButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+});
+
+// Detecting keyboard press
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+
+    default:
+      console.log("Invalid key!");
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
